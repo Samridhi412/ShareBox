965cd0729983291d8be7cb75a8efde023cc7897d
samridhi <samridhigarg2K@gmail.com>
2023-07-10 21:54:18 +0530

    changes

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const DBConnection = async () => {
-    const DB_URL = `mongodb://EagerCoder:EagerCoder123123@ac-9uabfny-shard-00-00.xc4lsmw.mongodb.net:27017,ac-9uabfny-shard-00-01.xc4lsmw.mongodb.net:27017,ac-9uabfny-shard-00-02.xc4lsmw.mongodb.net:27017/?ssl=true&replicaSet=atlas-ihvasx-shard-0&authSource=admin&retryWrites=true&w=majority`
-    try{
-        await mongoose.connect(DB_URL, { useNewUrlParser: true });
-        console.log('Database connected successfully')
-    }catch(error){
-        console.error('Error while connecting with the database', error.message);
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+
+    const MONGO_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-9uabfny-shard-00-00.xc4lsmw.mongodb.net:27017,ac-9uabfny-shard-00-01.xc4lsmw.mongodb.net:27017,ac-9uabfny-shard-00-02.xc4lsmw.mongodb.net:27017/?ssl=true&replicaSet=atlas-ihvasx-shard-0&authSource=admin&retryWrites=true&w=majority`
+    try {
+        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
     }
 }
 
 export default DBConnection;
-

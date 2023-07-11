5907bf39e5c1b8a77912cbc513b6256754a68d7f
samridhi <samridhigarg2K@gmail.com>
2023-07-11 12:04:35 +0530

    firs

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import router from './routes/routes.js';
 import cors from 'cors';
 import DBConnection from './database/db.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-const corsoption = {
-    origin: "https://sharex-qwzz.onrender.com"
-}
+// const corsoption = {
+//     origin: "https://sharex-qwzz.onrender.com"
+// }
 app.use(cors());
 app.use('/', router);
-const port = 8000;
+const port = process.env.PORT || 8000;
 DBConnection(); 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
